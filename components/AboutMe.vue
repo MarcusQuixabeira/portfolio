093a627cf31121c6c22d9496d4eba6d7482d52e7
MarcusQuixabeira <mquixaba@gmail.com>
2023-02-13 11:18:06 -0300

    Adding read more button to about me section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
-
 interface AboutMe {
   title: string,
-  text: string
+  text: string,
+  text2: string
 }
 
-defineProps<{ data: AboutMe }>()
+const aboutMe = defineProps<{ data: AboutMe }>()
+const readMore = ref(false);
+
+const readingText = computed(() => {
+  if (readMore.value) {
+    return aboutMe.data.text + `<span class="additional-text">${aboutMe.data.text2}<\p>`
+  }
+  return aboutMe.data.text
+})
+
+const readMoreLabel = computed(() => {
+  if (readMore.value) {
+    return isEnglish() ? 'Read less' : 'Ler menos'
+  }
+  return isEnglish() ? 'Read more' : 'Ler mais'
+})
 </script>
 
 <template>
   <section class="about-me">
     <h2>{{ data.title }}</h2>
-    <p>{{ data.text }}</p>
+    <p v-html="readingText"></p>
+    <p class="read-more" @click="readMore = !readMore">({{ readMoreLabel }})</p>
   </section>
 </template>
 
@@ -24,12 +40,14 @@ defineProps<{ data: AboutMe }>()
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
   h2 {
     margin: 0px;
     text-align: center;
     font-weight: 700;
     font-size: 24px;
   }
+
   p {
     margin: 0px;
     font-size: 22px;
@@ -37,6 +55,26 @@ defineProps<{ data: AboutMe }>()
     text-align: justify;
     padding-inline: 10%;
   }
+
+  @keyframes smooth {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 100;
+    }
+  }
+
+  :deep(.additional-text) {
+    animation: smooth 1s ease-in;
+  }
+  
+  .read-more {
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+
 }
 
 @media (max-width: 1080px) {
@@ -48,6 +86,7 @@ defineProps<{ data: AboutMe }>()
       font-size: 1.2rem;
       text-align: center;
     }
+
     p {
       font-size: 1.0rem;
       padding-inline: 5%;
