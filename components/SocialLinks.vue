2f1708d20010447eee49b5a615ec991280b7d348
MarcusQuixabeira <mquixaba@gmail.com>
2025-04-21 12:22:37 -0300

    Adding resume download button

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -4,9 +4,34 @@ const data = reactive(await fetchSocials())
 
 <template>
   <div class="social-links">
-    <a :href="data.linkedinUrl" target="_blank"><nuxt-icon name="mdi_linkedin" /></a>
-    <a :href="data.githubUrl" target="_blank"><nuxt-icon name="jam_github" /></a>
-    <a :href="`mailto:${data.email}`" target="_blank"><nuxt-icon name="mdi_email" /></a>
+    <a
+      :href="data.linkedinUrl"
+      target="_blank"
+      :title="data[getLanguage()].linkedinTitle"
+    >
+      <nuxt-icon name="mdi_linkedin" />
+    </a>
+    <a
+      :href="data.githubUrl"
+      target="_blank"
+      :title="data[getLanguage()].githubTitle"
+    >
+      <nuxt-icon name="jam_github" />
+    </a>
+    <a
+      :href="data.resumeUrl"
+      target="_blank"
+      :title="data[getLanguage()].resumeTitle"
+    >
+      <nuxt-icon name="mdi_file_document" />
+    </a> 
+    <a
+      :href="`mailto:${data.email}`"
+      target="_blank"
+      :title="data[getLanguage()].emailTitle"
+    >
+      <nuxt-icon name="mdi_email" />
+    </a>
   </div>
 </template>
 
